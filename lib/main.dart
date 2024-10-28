7fe04a279d3454b9163cb3b16c7f4df2e316e08a
Sersoc <45021157+Sersoc@users.noreply.github.com>
2024-10-29 00:53:54 +0900

    Add AudioPlayer Package && Made Event display sound when touch Text Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
+// import 'dart:async';
 import 'package:flutter/material.dart';
-
-void main() => runApp(XylophoneApp());
-
+import 'package:audioplayers/audioplayers.dart';
+  
+void main(){
+  return runApp(
+    XylophoneApp(),
+    );
+} 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Center(
+
+              child: TextButton(onPressed: (){
+final AudioPlayer player = AudioPlayer();
+                // print('')
+                // player.setSource(AssetSource(
+                player.setVolume(0.5);
+                player.play(AssetSource('note1.wav'));
+                
+              }, 
+              child:Text(
+              'nouns.ft',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black
+              ),
+            ), )
+              
+            )
+            
+          ),
         ),
       ),
     );
   }
 }
+// class XylophoneApp extends StatelessWidget {
+  
+// }
